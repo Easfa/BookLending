6717b29e657f19a201a39448ff16cb6b6a7a61ae
Batuhan Akçagöz <akcagozbatuhan@gmail.com>
2022-12-07 12:31:24 +0300

    Lend Barrow NavBar buttons created

---
+++ BookLending/Pages/Transaction/Barrow.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookLending.Pages.Transaction
+{
+    public class BarrowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BookLending/Pages/Transaction/Lend.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookLending.Pages.Transaction
+{
+    public class LendModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
