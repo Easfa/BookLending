62761aa3e447657b771e7bd9c46e166856ebdb5e
Batuhan Akçagöz <akcagozbatuhan@gmail.com>
2022-12-07 12:37:53 +0300

    BarrowList LendList Navbar buttons created

---
+++ BookLending/Pages/Show/BarrowsList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookLending.Pages.Show
+{
+    public class BarrowsListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
