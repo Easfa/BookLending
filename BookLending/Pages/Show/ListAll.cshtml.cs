9818fc85bdd38207be8446417fc51d0bdf37fe16
Batuhan Akçagöz <akcagozbatuhan@gmail.com>
2022-12-08 11:02:27 +0300

    ListAll created

--- BookLending/Pages/Show/ListAll.cshtml.cs
+++ BookLending/Pages/Show/ListAll.cshtml.cs
@@ -1,3 +1,4 @@
+using BookLending.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,22 @@ namespace BookLending.Pages.Show
 {
     public class ListAllModel : PageModel
     {
+        public List<Model.Transaction> Transactions { get; set; }
+        private readonly BookDbContext _db;
+        public ListAllModel(BookDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            Transactions = _db.Transaction.ToList();
+
+            foreach (var trans in Transactions)
+            {
+                trans.Person = _db.Person.Where(x => x.P_ID == trans.P_ID).FirstOrDefault();
+                trans.Book = _db.Books.Where(x => x.B_ID == trans.B_ID).FirstOrDefault();
+            }
         }
     }
 }
