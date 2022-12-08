86170233b7cd7db6068d4b06a3f98e5fda8c9f0e
Batuhan Akçagöz <akcagozbatuhan@gmail.com>
2022-12-08 10:45:22 +0300

    Delete cs created

--- BookLending/Pages/Transaction/Delete.cshtml.cs
+++ BookLending/Pages/Transaction/Delete.cshtml.cs
@@ -1,3 +1,4 @@
+using BookLending.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,23 @@ namespace BookLending.Pages.Transaction
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        [BindProperty(SupportsGet = true)]
+        public int Tid { get; set; }
+
+        private readonly BookDbContext _db;
+
+        public DeleteModel(BookDbContext db)
         {
+            _db = db;
+        }
+
+        public IActionResult OnGet()
+        {
+            Model.Transaction trnrmv = _db.Transaction.Where(x => x.T_ID == Tid).FirstOrDefault();
+            _db.Transaction.Remove(trnrmv);
+            _db.SaveChanges();
+
+            return RedirectToPage("/Index");
         }
     }
 }
