ca88779c8ca3424d90b381fdc25d442bcae3d3f2
Batuhan Akçagöz <akcagozbatuhan@gmail.com>
2022-12-08 10:50:19 +0300

    Lend cs created

--- BookLending/Pages/Transaction/Lend.cshtml.cs
+++ BookLending/Pages/Transaction/Lend.cshtml.cs
@@ -1,3 +1,4 @@
+using BookLending.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,23 @@ namespace BookLending.Pages.Transaction
 {
     public class LendModel : PageModel
     {
-        public void OnGet()
+        [BindProperty(SupportsGet = true)]
+        public int Tid { get; set; }
+
+        private readonly BookDbContext _db;
+
+        public LendModel(BookDbContext db)
         {
+            _db = db;
+        }
+
+        public IActionResult OnGet()
+        {
+            var trnrmv = _db.Transaction.Where(x => x.T_ID == Tid).FirstOrDefault();
+            trnrmv.LendingDate = DateTime.Now;
+            _db.SaveChanges();
+
+            return RedirectToPage("/Index");
         }
     }
 }
