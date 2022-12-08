c86d51fb8c74ba5ed6c737dcfdd33b417891dbbb
Batuhan Akçagöz <akcagozbatuhan@gmail.com>
2022-12-08 10:52:29 +0300

    LendsList created

--- BookLending/Pages/Show/LendsList.cshtml.cs
+++ BookLending/Pages/Show/LendsList.cshtml.cs
@@ -1,3 +1,4 @@
+using BookLending.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,22 @@ namespace BookLending.Pages.Show
 {
     public class LendsListModel : PageModel
     {
+        public List<Model.Transaction> Transactions { get; set; }
+        private readonly BookDbContext _db;
+        public LendsListModel( BookDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            Transactions = _db.Transaction.Where(x => x.LendingDate != null).ToList();
+
+            foreach (var trans in Transactions)
+            {
+                trans.Person = _db.Person.Where(x => x.P_ID == trans.P_ID).FirstOrDefault();
+                trans.Book = _db.Books.Where(x => x.B_ID == trans.B_ID).FirstOrDefault();
+            }
         }
     }
 }
